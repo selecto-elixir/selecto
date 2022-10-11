8371f23845a7ad2d7f46226ea59e6047845e9f9d
Chris Rohlfs <seeken@gmail.com>
2022-10-10 21:21:29 -0600

    stub

--- lib/listable/components.ex
+++ lib/listable/components.ex
@@ -5,7 +5,23 @@ defmodule Listable.Components do
   def view_panel(assigns) do
     ~H"""
       <div>
-      HERE <%= @listable.repo %>
+      View <%= @listable.repo %>
+      </div>
+    """
+  end
+
+  def filter_panel(assigns) do
+    ~H"""
+      <div>
+      Filter <%= @listable.repo %>
+      </div>
+    """
+  end
+
+  def results_panel(assigns) do
+    ~H"""
+      <div>
+      Results <%= @listable.repo %>
       </div>
     """
   end
