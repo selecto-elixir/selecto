ea499ddd5732641ea1be5a47dc8ababbb38b0bd8
Chris Rohlfs <seeken@gmail.com>
2022-12-06 21:24:02 -0700

    humanize names

--- lib/selecto/schema/column.ex
+++ lib/selecto/schema/column.ex
@@ -53,7 +53,7 @@ defmodule Selecto.Schema.Column do
         end
       )
 
-    name = Map.get(config, :name, field)
+    name = Map.get(config, :name, humanize(field))
 
     col = {
       colid,
@@ -70,4 +70,19 @@ defmodule Selecto.Schema.Column do
       )
     }
   end
+
+  #stolen from phoenix :D
+  def humanize(atom) when is_atom(atom),
+    do: humanize(Atom.to_string(atom))
+  def humanize(bin) when is_binary(bin) do
+    bin =
+      if String.ends_with?(bin, "_id") do
+        binary_part(bin, 0, byte_size(bin) - 3)
+      else
+        bin
+      end
+
+    bin |> String.replace("_", " ") |> String.capitalize
+  end
+
 end
