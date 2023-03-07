092f3b3b2fb4d7eecc9bb4a70eb06b1b4ee70e7d
Chris Rohlfs <seeken@gmail.com>
2023-03-07 15:53:20 -0700

    replace cehck safe phrase

--- lib/selecto/helpers.ex
+++ lib/selecto/helpers.ex
@@ -1,4 +1,13 @@
 defmodule Selecto.Helpers do
 
+  def check_safe_phrase(string) do
+    if String.length(string) < 1 or String.match?(string, ~r/[^a-zA-Z0-9_ ]/) do
+      raise RuntimeError, message: "Invalid String #{string}"
+      false
+    else
+      string
+    end
+  end
+
 
 end
