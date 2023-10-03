f6cad930e2367508a131e94352e5bfc0d304f9ea
Chris Rohlfs <seeken@gmail.com>
2023-10-02 18:19:08 -0600

    Ecto 3.10

--- mix.exs
+++ mix.exs
@@ -25,8 +25,8 @@ defmodule Selecto.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.9.4"},
-      {:ecto_sql, "~> 3.9"},
+      {:ecto, "~> 3.10"},
+      {:ecto_sql, "~> 3.10"},
       {:uuid, "~> 1.1"},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
       {:timex, "~> 3.7.9"},
